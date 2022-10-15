3565564d7ef86f127eb8e3522c305821b8eac743
RichieBlack <box2045@gmail.com>
2022-10-15 17:22:03 +0400

    solution

---
+++ 8-kyu/Basic Training - Add item to an Array.py
@@ -0,0 +1,21 @@
+# link: https://www.codewars.com/kata/reviews/5758661640292ad941000002/groups/6204ef162b0f5f0001079e7c
+
+websites.append("codewars")
+
+
+'''
+Best practices
+--------------
+
+websites.append("codewars")
+
+--------------
+
+websites += ["codewars"]
+
+--------------
+
+websites.insert(0, "codewars")
+
+--------------
+'''
