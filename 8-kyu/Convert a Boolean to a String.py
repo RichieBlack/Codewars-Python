67165a68b221987c44628637b213807015a7ea22
RichieBlack <box2045@gmail.com>
2022-10-15 09:36:17 +0400

    solution

---
+++ 8-kyu/Convert a Boolean to a String.py
@@ -0,0 +1,22 @@
+# link: https://www.codewars.com/kata/551b4501ac0447318f0009cd/python
+
+def boolean_to_string(b):
+    return str(b)
+
+'''
+Best practices
+--------------
+
+def boolean_to_string(b):
+    return str(b)
+
+--------------
+
+boolean_to_string = str
+
+--------------
+
+
+
+--------------
+'''
