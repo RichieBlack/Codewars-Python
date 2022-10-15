df42d7984efcdf24d94e31f95a1ce5b6bcf1ecd8
RichieBlack <box2045@gmail.com>
2022-10-15 09:45:51 +0400

    solution

---
+++ 8-kyu/Convert a Number to a String.py
@@ -0,0 +1,22 @@
+# link: https://www.codewars.com/kata/reviews/54272ba8e80d824cb80001f7/groups/61d3f28168975c0001d89693
+
+def number_to_string(num):
+    return (str(num))
+
+'''
+Best practices
+--------------
+
+def number_to_string(num):
+    return str(num)
+
+--------------
+
+number_to_string = lambda n: str(n)
+
+--------------
+
+number_to_string = str
+
+--------------
+'''
