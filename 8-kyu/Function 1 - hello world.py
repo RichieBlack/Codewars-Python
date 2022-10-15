cb26c14e0eaeaaeeed24974da8755d29d9a33556
RichieBlack <box2045@gmail.com>
2022-10-15 09:29:31 +0400

    solution

---
+++ 8-kyu/Function 1 - hello world.py
@@ -0,0 +1,18 @@
+# link: https://www.codewars.com/kata/523b4ff7adca849afe000035/python
+
+def greet():
+    return "hello world!"
+
+'''
+Best practices
+--------------
+
+def greet():
+    return "hello world!"
+
+--------------
+
+greet = lambda: "hello world!"
+
+--------------
+'''
