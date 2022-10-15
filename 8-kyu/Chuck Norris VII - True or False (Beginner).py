9717763693ded6c961e88d196469a2548f44ced3
RichieBlack <box2045@gmail.com>
2022-10-15 19:45:53 +0400

    solution

---
+++ 8-kyu/Chuck Norris VII - True or False (Beginner).py
@@ -0,0 +1,30 @@
+# link: https://www.codewars.com/kata/reviews/581a29ffe2689af76e0000ad/groups/61fd0f9d64a4c200019e88ce
+
+def if_chuck_says_so():
+    return not True
+
+
+# I have been studying Python for one month.
+
+'''
+Best practices
+--------------
+
+def ifChuckSaysSo():
+    return 0
+
+--------------
+
+def ifChuckSaysSo():
+    return not True
+
+--------------
+
+ifChuckSaysSo = int
+
+--------------
+
+ifChuckSaysSo = lambda: 0
+
+--------------
+'''
