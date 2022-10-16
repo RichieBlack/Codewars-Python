4d015559605a86fec1e607a75751dcc77bf88282
RichieBlack <box2045@gmail.com>
2022-10-16 15:37:12 +0400

    solution

---
+++ 8-kyu/You Can't Code Under Pressure 1.py
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/reviews/5411f6d717dc0b0f8700021b/groups/5bfd523129354d4124001855
+
+def double_integer(i):
+    return i * 2
+
+
+# I have been studying Python for one month.
+
+"""
+---------------
+Best practices
+---------------
+
+def doubleInteger(i):
+    return i * 2
+
+---------------
+
+def doubleInteger(i):
+    return i << 1
+
+---------------
+
+doubleInteger = lambda i: i * 2
+
+---------------
+"""
