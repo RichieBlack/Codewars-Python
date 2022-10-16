e2967a46c620f181a20515af23cd27ea37106f96
RichieBlack <box2045@gmail.com>
2022-10-16 06:09:15 +0400

    solution

---
+++ 8-kyu/Opposite number.py
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/reviews/56deebdf6a5c28baa900003b/groups/56df04cd9905a1b162000407
+
+def opposite(number):
+    return number * -1
+
+
+# I have been studying Python for one month.
+
+"""
+--------------
+Best practices
+--------------
+
+def opposite(number):
+    return -number
+
+--------------
+
+def opposite(number):
+  return number * -1
+
+--------------
+
+def opposite(number):
+    return number - number * 2
+
+--------------
+"""
