db670f88ed6a480566e20ceef54339cfc8eab1ba
RichieBlack <box2045@gmail.com>
2022-10-16 06:14:38 +0400

    solution

---
+++ 8-kyu/Reversed Strings.py
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/reviews/54e10e357775b79e2800011c/groups/54f15002d4911279c6000a89
+
+def solution(string):
+    return string[::-1]
+
+
+# I have been studying Python for one month.
+
+"""
+---------------
+Best practices
+---------------
+
+def solution(str):
+  return str[::-1]
+
+---------------
+
+solution = lambda s: s[::-1]
+
+---------------
+
+def solution(string):
+    return ''.join(i for i in reversed(string))
+
+---------------
+"""
