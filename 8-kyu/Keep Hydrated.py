973676bdf3479138a99716dfb8e0a480a17ea42d
RichieBlack <box2045@gmail.com>
2022-10-16 15:25:41 +0400

    solution

---
+++ 8-kyu/Keep Hydrated.py
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/reviews/582e00ba4b1368af7b0000b3/groups/582e06a1aeb69ac94f000057
+
+def litres(time):
+    return int(time * 0.5)
+
+
+# I have been studying Python for one month.
+
+"""
+---------------
+Best practices
+---------------
+
+def litres(time):
+    return time // 2
+
+---------------
+
+def litres(time):
+    return int(time * 0.5)
+
+---------------
+
+def litres(time):
+    return 'Are you Tom Cruise?' if time < 0 else int(time / 2)
+
+---------------
+"""
